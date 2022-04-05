6d0caa03a48e6259f28af976481b460be4df5bc2
RizqiAlda <15200349@bsi.ac.id>
2022-04-05 16:40:03 +0700

    init

---
+++ application/views/v_index.php
@@ -0,0 +1,14 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p align='justify'>Rizqi Nur Esmeralda</p>
+    <p> Edy Supriyadi </p>
+    <p> Nuridah </p>
+    <p> Mia Azzahra </p>
+    <ol type="a">
+        <li>Model</li>
+        <p align='justify'>Muhammmad Alvaro Adhitama Putra Harahap</p>
+        <li>View</li>
+        <p align='justify'>Raga Fasya Ghany</p>
+        <li>Controller</li>
+        <p align='justify'>Muhammad Ridwan Arifin Harahap</p>
+</section>
